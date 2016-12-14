051f8eded4f5fb5d8d0c95b16d5306eef4c59978
ronaldo.pedro <ronaldo.pedro@VF-TIl01.VIAFERTIL.LOCAL>
2016-12-14 15:39:53 -0300

    Atualizado

---
+++ src/dao/EquipamentoDAO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.EquipamentoBean;
+import factory.ConexaoFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ronaldo.pedro
+ */
+public class EquipamentoDAO {
+
+    private Statement stmt;
+    private ResultSet rsNavegar;
+    private java.sql.ResultSet rsListar;
+    private Connection con;
+
+    public boolean inserir(EquipamentoBean equipamento) {
+
+        try {
+
+            con = ConexaoFactory.getConnection();
+            String sql = "INSERT INTO cad_equipamento(descricao,modelo,num_serie,marca,processador,frequencia,ram,hd,filial,departamento,usuario,observacao,ult_verificacao) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            System.out.println("SQL");
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, equipamento.getDescricao());
+            ps.setString(2, equipamento.getModelo());
+            ps.setString(3, equipamento.getNum_serie());
+            ps.setString(4, equipamento.getMarca());
+            ps.setString(5, equipamento.getProcessador());
+            ps.setString(6, equipamento.getFrequencia());
+            ps.setString(7, equipamento.getRam());
+            ps.setString(8, equipamento.getHd());
+            ps.setString(9, equipamento.getFilial());
+            ps.setString(10, equipamento.getDepartamento());
+            ps.setString(11, equipamento.getUsuario());
+            ps.setString(12, equipamento.getObservacao());
+            ps.setObject(13, equipamento.getUlt_verificacao());
+
+            JOptionPane.showMessageDialog(null, "Equipamento do Usuario " + equipamento.getUsuario() + " Cadastrado com sucesso! ");
+
+            return ps.executeUpdate() != PreparedStatement.EXECUTE_FAILED;
+
+        } catch (SQLException e) {
+
+            return false;
+        } catch (ClassNotFoundException e) {
+
+            return false;
+        }
+    }
+}
---
+++ src/dao/FilialDAO.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.EquipamentoBean;
+import bean.FilialBean;
+import factory.ConexaoFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ronaldo.pedro
+ */
+public class FilialDAO {
+
+    private Statement stmt;
+    private ResultSet rsNavegar;
+    private java.sql.ResultSet rsListar;
+    private Connection con;
+
+    public boolean inserir(FilialBean filial) {
+
+        try {
+
+            con = ConexaoFactory.getConnection();
+            String sql = "INSERT INTO cad_filial(cnpj,inscricao_estadual,razao_social,endereco,numero,bairro,cep,cidade,telefone,uf) VALUES (?,?,?,?,?,?,?,?,?,?)";
+            System.out.println("SQL");
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, filial.getCnpj());
+            ps.setString(2, filial.getInscricao_estadual());
+            ps.setString(3, filial.getRazao_social());
+            ps.setString(4, filial.getEndereco());
+            ps.setString(5, filial.getNumero());
+            ps.setString(6, filial.getBairro());
+            ps.setString(7, filial.getCep());
+            ps.setString(8, filial.getCidade());
+            ps.setString(9, filial.getTelefone());
+            ps.setString(10, filial.getUf());
+
+            JOptionPane.showMessageDialog(null, "Filial de " + filial.getCidade() + " Cadastrada com sucesso! ");
+
+            return ps.executeUpdate() != PreparedStatement.EXECUTE_FAILED;
+
+        } catch (SQLException e) {
+
+            return false;
+        } catch (ClassNotFoundException e) {
+
+            return false;
+        }
+    }
+    
+    public boolean altera(FilialBean filial) throws ClassNotFoundException {
+        try {
+         
+            con = ConexaoFactory.getConnection();
+            Statement stmt = con.createStatement();
+//            stmt.executeUpdate("UPDATE alunos SET nome='"
+//                    + aluno.getNome() + "',cpf='" + aluno.getCpf() + "',email='" + aluno.getEmail() + "',telefone='" + aluno.getTelefone() + "',periodo='" + aluno.getPeriodo() + "',serie='" + aluno.getSerie() + "',turma='" + aluno.getTurma() + "' WHERE cpf='" + aluno.getCpf() + "'");
+//            JOptionPane.showMessageDialog(null, " Filial Atualizada com Sucesso!!");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+
+        }
+        return true;
+    }
+    
+}
